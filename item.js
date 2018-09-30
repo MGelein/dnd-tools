e07e9c8eab7bae10bff23fe383612f113d27fa45
Mees Gelein <meesgelein@gmail.com>
2018-09-30 17:16:16 +0200

    Let's start with the more feasible treasure generator

---
+++ item.js
@@ -0,0 +1,8 @@
+/**Store all command line parameters in a separate object */
+const args = process.argv.splice(2);
+
+/**
+ * For an item to be generated we can give it
+ * - Category (weapon, potion, scroll, etc)
+ * - Rarity ()
+ */
