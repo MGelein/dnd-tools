9662c81e150850a5a2117fbdafdd0c07659f51b3
Mees Gelein <meesgelein@gmail.com>
2018-09-30 20:52:06 +0200

    Art is now also a separate file

---
+++ art.js
@@ -0,0 +1,38 @@
+/**
+ * Contains all the art tables from the DMG
+ */
+
+/**
+ * ART
+ */
+const art25 = ["Silver ewer", "Carved bone statuette", "Small gold bracelet", "Cloth-of-gold vestments", "Black velvet mask stitched with silver thread", "Copper chalice with silver filligree", "Pair of engraved bone dice", "Small mirror set in a painted wooden frame", "Embroidered silk handkerchief", "Gold locket with a painted portrait inside"];
+const art250 = ["Gold ring set with bloodstones", "Carved ivory statuette", "Large gold bracelet", "Silver necklace with a gemstone pendant", "Bronze crown", "Silk robe", "Large well-made tapestry", "Brass mug with a jade inlay", "Box of turquoise animal figurines", "Gold bird cage with electrum filigree"];
+const art750 = ["Silver chalice set with moonstones", "Silver-plated steel longsword with jet set in hilt", "Carved harp of exotic wood with ivory inlay and zircon gems", "Small gold idol", "Gold dragon comb set with red garnets as eyes", "Bottle stopper cork embossed with gold leaf and set with amethysts", "Ceremonial electrum dagger with a black pearl in the pommel", "Silver and gold brooch", "Obsidian statuette with gold fittings and inlay", "Painted gold war mask"];
+const art2500 = ["Fine gold chain set with a fire opal", "Old masterpiece painting", "Embroidered silk and velvet mantle set with numerous moonstones", "Platinum bracelet set with a sapphire", "Embroidered glove set with jewel chips", "Jeweled anklet", "Gold music box", "Gold circlet set with four aquamarines", "Eye patch with a mock eye set in blue sapphire and moonstone", "A necklace string of small pink pearls"];
+const art7500 = ["Jeweled gold crown", "Jeweled platinum ring", "Small gold statuette set with rubies", "Gold cup set with emerals", "Gold jewelry box with platinum filigree", "Painted gold child's sarcophagus", "Jade game board with solid gold playing pieces", "Bejeweled ivory drinking horn with gold filigree"];
+
+/**
+* Returns a random option from an array
+* @param {Array} array 
+*/
+function rnd(array) {
+    return array[Math.floor((Math.random() * array.length))];
+}
+
+/**
+ * Returns a string that represents what we give
+ * @param {int} worth 
+ * @param {int} amount 
+ */
+exports.art = function(worth, amount){
+    //All the parts of the art stuff together
+    let parts = [];
+    //Pick the right artList based on worth
+    let artList = (worth == 7500) ? art7500 : (worth == 2500) ? art2500 : (worth == 750) ? art750 : (worth == 250) ? art250 : art25;
+    //Now pick untill amount < 0
+    while(amount > 0){
+        parts.push(rnd(artList));
+        amount --;
+    }
+    return parts.join("\n");
+}
--- treasure.js
+++ treasure.js
@@ -3,7 +3,9 @@ const args = process.argv.splice(2);
 /**The executeSync command */
 const exec = require("child_process").execSync;
 /**Gem generation js file */
-const gems = require('./gems').gems;
+const {gems} = require('./gems');
+/**Art generation js file */
+const {art} = require('./art');
 
 //Currency const
 const GP = "Gp";
@@ -17,14 +19,6 @@ const EPn = EP + " and ";
 const SPn = SP + " and ";
 const CPn = CP + " and ";
 
-/**
- * ART
- */
-const art25 = ["Silver ewer", "Carved bone statuette", "Small gold bracelet", "Cloth-of-gold vestments", "Black velvet mask stitched with silver thread", "Copper chalice with silver filligree", "Pair of engraved bone dice", "Small mirror set in a painted wooden frame", "Embroidered silk handkerchief", "Gold locket with a painted portrait inside"];
-const art250 = ["Gold ring set with bloodstones", "Carved ivory statuette", "Large gold bracelet", "Silver necklace with a gemstone pendant", "Bronze crown", "Silk robe", "Large well-made tapestry", "Brass mug with a jade inlay", "Box of turquoise animal figurines", "Gold bird cage with electrum filigree"];
-const art750 = ["Silver chalice set with moonstones", "Silver-plated steel longsword with jet set in hilt", "Carved harp of exotic wood with ivory inlay and zircon gems", "Small gold idol", "Gold dragon comb set with red garnets as eyes", "Bottle stopper cork embossed with gold leaf and set with amethysts", "Ceremonial electrum dagger with a black pearl in the pommel", "Silver and gold brooch", "Obsidian statuette with gold fittings and inlay", "Painted gold war mask"];
-const art2500 = ["Fine gold chain set with a fire opal", "Old masterpiece painting", "Embroidered silk and velvet mantle set with numerous moonstones", "Platinum bracelet set with a sapphire", "Embroidered glove set with jewel chips", "Jeweled anklet", "Gold music box", "Gold circlet set with four aquamarines", "Eye patch with a mock eye set in blue sapphire and moonstone", "A necklace string of small pink pearls"];
-const art7500 = ["Jeweled gold crown", "Jeweled platinum ring", "Small gold statuette set with rubies", "Gold cup set with emerals", "Gold jewelry box with platinum filigree", "Painted gold child's sarcophagus", "Jade game board with solid gold playing pieces", "Bejeweled ivory drinking horn with gold filigree"];
 
 //This variable will hold the size specification, by default individual
 let size = "individual";
@@ -47,7 +41,6 @@ args.forEach(function (argument) {
  * The line that does the actual work
  */
 generateTreasure();
-console.log(gems(10, 3));
 
 /**
  * Generates the actual treasure
@@ -334,22 +327,3 @@ function d10(times) { return dice(10, times) };
 function d12(times) { return dice(12, times) };
 function d20(times) { return dice(20, times) };
 function d100(times) { return dice(100, times) };
-
-
-/**
- * Returns a string that represents what we give
- * @param {int} worth 
- * @param {int} amount 
- */
-function art(worth, amount){
-    //All the parts of the art stuff together
-    let parts = [];
-    //Pick the right artList based on worth
-    let artList = (worth == 7500) ? art7500 : (worth == 2500) ? art2500 : (worth == 750) ? art750 : (worth == 250) ? art250 : art25;
-    //Now pick untill amount < 0
-    while(amount > 0){
-        parts.push(rnd(artList));
-        amount --;
-    }
-    return parts.join("\n");
-}
