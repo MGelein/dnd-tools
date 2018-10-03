4deff3f5e7329d993325ed8fd4ea877bffa895c5
Mees Gelein <meesgelein@gmail.com>
2018-10-03 11:22:36 +0200

    First work done on the shop generator

--- shop.js
+++ shop.js
@@ -1,2 +1,8 @@
+//Import shop module
+const {shop} = require('./lib/shops');
+
 /**Store all command line parameters in a separate object */
 const args = process.argv.splice(2);
+
+//Pass forward the command and log the output
+console.log(shop(args.join(" ")));
