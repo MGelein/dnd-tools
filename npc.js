c56525d989a29bc18edbeab5833ac53b477f4271
Mees Gelein <meesgelein@gmail.com>
2018-10-18 15:01:03 +0200

    removed color statement, maybe use chalk later?

--- npc.js
+++ npc.js
@@ -1,8 +1,7 @@
 const {npc} = require('./lib/npcs');
-var colors = require('colors');
 
 /**Store all command line parameters in a separate object */
 const args = process.argv.splice(2);
 
 //Log the result from the npc creation call
-console.log(npc(args.join(" ")).green);
+console.log(npc(args.join(" ")));
