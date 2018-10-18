28983f163093b9508774bab16d7111168af7806e
Mees Gelein <meesgelein@gmail.com>
2018-10-18 13:54:03 +0200

    Changes to NPCS and added market stalls to shops

--- npc.js
+++ npc.js
@@ -1,7 +1,8 @@
 const {npc} = require('./lib/npcs');
+var colors = require('colors');
 
 /**Store all command line parameters in a separate object */
 const args = process.argv.splice(2);
 
 //Log the result from the npc creation call
-console.log(npc(args.join(" ")));
+console.log(npc(args.join(" ")).green);
